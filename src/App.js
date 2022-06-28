6f45330870e7f480d390df35a37c4861969074be
Sam Khan <samk.code@gmail.com>
2022-06-28 12:05:58 -0400

    Feat: Serachbar and write app.Css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+import {useEffect} from 'react';
 import './App.css';
+import SearchIcon from './search.svg'
+const API_URL = 'http://www.omdbapi.com?apikey=edf7b039'
 
 function App() {
+    const serachMovies = async (title)=>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+ useEffect(()=>{
+    serachMovies('spiderman');
+ },[])
   return (
-    <div className="App">
-      <h1>Test
-      </h1>
+    <div className='app'>
+      <h1>Movie Land</h1>
+      <div className='search'>
+        <input placeholder='Search for movies'
+         value="siperman"
+         onChange={()=>{}}
+        />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+//Here is your key: edf7b039
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
